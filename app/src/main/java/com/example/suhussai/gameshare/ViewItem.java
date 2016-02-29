4145d2993129ca855652cfa8fe91935582adc391
Daniel Mitchell <dan@uws105357usr.uws.ualberta.ca>
2016-02-29 11:37:47 -0700

    Added delete button with code (UC 15)

--- app/src/main/java/com/example/suhussai/gameshare/ViewItem.java
+++ app/src/main/java/com/example/suhussai/gameshare/ViewItem.java
@@ -1,5 +1,8 @@
 package com.example.suhussai.gameshare;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,10 +21,14 @@ public class ViewItem extends AppCompatActivity{
     private EditText TimeReq;
     private EditText Platform;
 
+    private Item item;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item);
+
+        //TODO: set item to be the Item object being displayed when view is loaded (could be done on controller side)
 
         GameName = (EditText) findViewById(R.id.AddItem_NameEdit);
         Players = (EditText) findViewById(R.id.AddItem_PlayersEdit);
@@ -78,6 +85,35 @@ public class ViewItem extends AppCompatActivity{
                 } catch (Exception e) {
 
                 }
+            }
+        });
+
+        Button DeleteButton = (Button) findViewById(R.id.AddItem_Delete);
+
+        //TODO probably a better way to do this
+        //'this' needs to be accessed by AlertDialog.Builder, but it is inside an OnClickListener
+        //so the this keyword is overwritten
+        final Context holder = this;
+
+        DeleteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder adBuilder = new AlertDialog.Builder(holder);
+                adBuilder.setMessage("Are you sure you want to delete this item?");
+                adBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //TODO modify this so the user is known at this stage. Using a test user in interim.
+                        User user = new User("testuser", "testpass");
+                        user.deleteItem(item);
+                    }
+                });
+                adBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                AlertDialog ad = adBuilder.create();
+                ad.show();
             }
         });
     }
